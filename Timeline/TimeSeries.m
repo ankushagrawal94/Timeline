758bddd87b95b16196950861c363d105cc0d2532
ankushagrawal94 <ankushagrawal94@yahoo.com>
2015-02-21 08:38:31 -0800

    Updated. Still not working 100%

--- Timeline/TimeSeries.m
+++ Timeline/TimeSeries.m
@@ -11,28 +11,39 @@
 @implementation TimeSeries
 
 - (id) initWithEvents:(NSArray*)eventArray {
+    
+    NSDictionary *dict1 = @{@"artist": @"The Colorist", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:123456789], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:223456789]};
+    NSArray *test = @[dict1]; //replace all eventArray with test
+    
     NSDate * startDate = [[NSDate alloc] initWithTimeIntervalSince1970:INTMAX_MAX];
     NSDate * endDate = [[NSDate alloc] initWithTimeIntervalSince1970:0];
     
     for (int i = 0; i < eventArray.count; i++) {
         if (eventArray[i][@"start"] < startDate) {
-            startDate = eventArray[i][@"start"];
+            startDate = test[i][@"start"];
         }
         if (eventArray[i][@"end"] > endDate) {
-            endDate = eventArray[i][@"end"];
+            endDate = test[i][@"end"];
         }
     }
     
     NSTimeInterval distanceBetween = [startDate timeIntervalSinceDate:endDate];
     NSInteger minutesBetween = distanceBetween / 60;
     
-    /*NSMutableArray * events = [[NSMutableArray alloc] initWithCapacity:minutesBetween];
+    NSMutableArray * events = [[NSMutableArray alloc] initWithCapacity:minutesBetween]; //Each index value represents a minute
+    
     for (int i = 0; i < events.count; i++) {
-        if (eventArray[i][@"start"] == events[i].) {
-            
+        if (test[i][@"start"]/60  == i) {
+            events[i] = test[i];
         }
-    }*/
+        else {
+            events[i] = nil;
+        }
+    }
+    
     return self;
 }
 
 @end
+
+
