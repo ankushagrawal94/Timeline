283b7882a5a63aa132f3e9a22b97d6f3c6442ff3
ankushagrawal94 <ankushagrawal94@yahoo.com>
2015-02-21 19:30:58 -0800

    timeline update

--- Timeline/TimelineViewController.m
+++ Timeline/TimelineViewController.m
@@ -23,16 +23,16 @@
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"red-o.jpg"]];
     
     NSDictionary *dict1 = @{@"artist": @"The Colorist", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424505600], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424506200], @"stage": @1, @"image": [UIImage imageNamed:@"blue-o.jpg"], @"duration": @10};
-    NSDictionary *dict2 = @{@"artist": @"The Colorist 2", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424506200], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424508000], @"stage": @1, @"image": [UIImage imageNamed:@"blueish.jpg"], @"duration": @30};
+    NSDictionary *dict2 = @{@"artist": @"The Colorist 2", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424506200], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424508000], @"stage": @1, @"image": [UIImage imageNamed:@"blueish.jpg"], @"duration": @45};
     NSDictionary *dict3 = @{@"artist": @"The Colorist 3", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424508000], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424508600], @"stage": @2, @"image": [UIImage imageNamed:@"heartbeat.jpg"], @"duration": @10};
-    NSDictionary *dict4 = @{@"artist": @"The Colorist 4", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424508600], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424514000], @"stage": @2, @"image": [UIImage imageNamed:@"hexagon.jpg"], @"duration": @90};
+    NSDictionary *dict4 = @{@"artist": @"The Colorist 4", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424508600], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424514000], @"stage": @2, @"image": [UIImage imageNamed:@"hexagon.jpg"], @"duration": @45};
     
     TimeSeries *ts = [[TimeSeries alloc] initWithEvents:@[dict1, dict2, dict3, dict4]];
     NSArray *keys = [ts.eventDict allKeys];
     for (NSString *key in keys){
         if (ts.eventDict[key]){
             EventView *event;
-            CGFloat height = [key integerValue] * (1.0/4.5);
+            CGFloat height = [key integerValue] * (1.0/18);
             if ([[ts.eventDict[key] objectForKey:@"stage"]  integerValue] == 1) {
                 event = [[EventView alloc] initWithFrame:CGRectMake(0, height, self.view.frame.size.width/2 + 19, self.view.frame.size.height)];
             }
