22da3be24151d99aaedb6e541ce9aec31a2eee7c
Matt Gabor <mattgabor@gmail.com>
2015-02-21 19:29:34 -0800

    backgrounds added

--- Timeline/Artwork/EventView.m
+++ Timeline/Artwork/EventView.m
@@ -27,47 +27,47 @@
             [VTimeline drawLeft_10WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life"];
         }
         else{
-            [VTimeline drawRight_10WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life 2"];
+            [VTimeline drawRight_10WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life NULL"];
             
         }
     }
     
     if ([self.durationNum isEqualToValue:@30]){
         if ([self.stageNum isEqualToNumber:@1]){
-            [VTimeline drawLeft_30WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life"];
+            [VTimeline drawRight_30WithArtistImage:[UIImage imageNamed:@"red-o.jpg"] imageY:70 imageSize:116 timeText:@"7:45" artistName:@"Kanye West"];
         }
         else{
-            [VTimeline drawRight_30WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life 2"];
+            [VTimeline drawRight_30WithArtistImage:[UIImage imageNamed:@"red-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Kanye West"];
             
         }
     }
     
     if ([self.durationNum isEqualToValue:@45]){
         if ([self.stageNum isEqualToNumber:@1]){
-            [VTimeline drawLeft_45WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life"];
+            [VTimeline drawLeft_45WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life3.5"];
         }
         else{
-            [VTimeline drawRight_45WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life 2"];
+            [VTimeline drawRight_45WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life 4"];
             
         }
     }
     
     if ([self.durationNum isEqualToValue:@60]){
         if ([self.stageNum isEqualToNumber:@1]){
-            [VTimeline drawLeft_1WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life"];
+            [VTimeline drawLeft_1WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life4.5"];
         }
         else{
-            [VTimeline drawRight_1WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life 2"];
+            [VTimeline drawRight_1WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life 5"];
             
         }
     }
     
     if ([self.durationNum isEqualToValue:@90]){
         if ([self.stageNum isEqualToNumber:@1]){
-            [VTimeline drawLeft_15WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life"];
+            [VTimeline drawLeft_15WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life 5.5"];
         }
         else{
-            [VTimeline drawRight_15WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life 2"];
+            [VTimeline drawRight_15WithArtistImage:[UIImage imageNamed:@"blueish-o.jpg"] imageY:60 imageSize:116 timeText:@"7:30" artistName:@"Dada Life 6"];
             
         }
     }
--- Timeline/TimelineViewController.m
+++ Timeline/TimelineViewController.m
@@ -20,14 +20,14 @@
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.width)];
     scrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height * 3);
     self.view = scrollView;
-    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"red-o.jpg"]];
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background-small.jpg"]];
     
     NSDictionary *dict1 = @{@"artist": @"The Colorist", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424505600], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424506200], @"stage": @1, @"image": [UIImage imageNamed:@"blue-o.jpg"], @"duration": @10};
-    NSDictionary *dict2 = @{@"artist": @"The Colorist 2", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424506200], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424508000], @"stage": @1, @"image": [UIImage imageNamed:@"blueish.jpg"], @"duration": @30};
-    NSDictionary *dict3 = @{@"artist": @"The Colorist 3", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424508000], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424508600], @"stage": @2, @"image": [UIImage imageNamed:@"heartbeat.jpg"], @"duration": @10};
-    NSDictionary *dict4 = @{@"artist": @"The Colorist 4", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424508600], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424514000], @"stage": @2, @"image": [UIImage imageNamed:@"hexagon.jpg"], @"duration": @90};
-    
-    TimeSeries *ts = [[TimeSeries alloc] initWithEvents:@[dict1, dict2, dict3, dict4]];
+    NSDictionary *dict2 = @{@"artist": @"The Colorist 2", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424506200], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424508000], @"stage": @2, @"image": [UIImage imageNamed:@"blueish.jpg"], @"duration": @30};
+//    NSDictionary *dict3 = @{@"artist": @"The Colorist 3", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424508000], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424508600], @"stage": @2, @"image": [UIImage imageNamed:@"heartbeat.jpg"], @"duration": @10};
+//    NSDictionary *dict4 = @{@"artist": @"The Colorist 4", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424508600], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424514000], @"stage": @2, @"image": [UIImage imageNamed:@"hexagon.jpg"], @"duration": @90};
+//    
+    TimeSeries *ts = [[TimeSeries alloc] initWithEvents:@[dict1, dict2]];
     NSArray *keys = [ts.eventDict allKeys];
     for (NSString *key in keys){
         if (ts.eventDict[key]){
