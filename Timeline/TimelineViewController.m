9e0403e6f78beaf751f78ecbd2c6429619076cbc
ankushagrawal94 <ankushagrawal94@yahoo.com>
2015-02-21 16:27:16 -0800

    Testing

--- Timeline/TimelineViewController.m
+++ Timeline/TimelineViewController.m
@@ -16,24 +16,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.width)];
-    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"red-o.jpg"]];
-    NSDictionary *dict1 = @{@"artist": @"The Colorist", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424568778], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424569778], @"stage": @1, @"image": [UIImage imageNamed:@"blue-o.jpg"]};
-    NSDictionary *dict2 = @{@"artist": @"The Colorist 2", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424569779], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424570878], @"stage": @2, @"image": [UIImage imageNamed:@"blueish.jpg"]};/*
-    NSDictionary *dict3 = @{@"artist": @"The Colorist 3", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424578778], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424579778], @"stage": @1, @"image": [UIImage imageNamed:@"heartbeat.jpg"]};
-    NSDictionary *dict4 = @{@"artist": @"The Colorist 4", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424589779], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424580878], @"stage": @2, @"image": [UIImage imageNamed:@"hexagon.jpg"]};*/
     
-    TimeSeries *ts = [[TimeSeries alloc] initWithEvents:@[dict1, dict2]];
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.width)];
+    scrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height * 3);
+    self.view = scrollView;
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"red-o.jpg"]];
+    
+    NSDictionary *dict1 = @{@"artist": @"The Colorist", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424505600], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424506200], @"stage": @1, @"image": [UIImage imageNamed:@"blue-o.jpg"]};
+    NSDictionary *dict2 = @{@"artist": @"The Colorist 2", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424506200], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424508000], @"stage": @1, @"image": [UIImage imageNamed:@"blueish.jpg"]};
+    NSDictionary *dict3 = @{@"artist": @"The Colorist 3", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424508000], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424508600], @"stage": @1, @"image": [UIImage imageNamed:@"heartbeat.jpg"]};
+    NSDictionary *dict4 = @{@"artist": @"The Colorist 4", @"start": [[NSDate alloc] initWithTimeIntervalSince1970:1424508600], @"end": [[NSDate alloc] initWithTimeIntervalSince1970:1424514000], @"stage": @1, @"image": [UIImage imageNamed:@"hexagon.jpg"]};
+    
+    TimeSeries *ts = [[TimeSeries alloc] initWithEvents:@[dict1, dict2, dict3, dict4]];
     NSArray *keys = [ts.eventDict allKeys];
     for (NSString *key in keys){
         if (ts.eventDict[key]){
             EventView *event;
-            NSLog(@"%@", [ts.eventDict[key] objectForKey:@"stage"]);
+            CGFloat height = [key integerValue] * (1.0/6);
             if ([[ts.eventDict[key] objectForKey:@"stage"]  integerValue] == 1) {
-                event = [[EventView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width/2, self.view.frame.size.height)];
+                event = [[EventView alloc] initWithFrame:CGRectMake(0, height, self.view.frame.size.width/2 + 19, self.view.frame.size.height)];
             }
             else {
-                event = [[EventView alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2 - 19, 0, self.view.frame.size.width/2 + 19, self.view.frame.size.height)];
+                event = [[EventView alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2 - 19, height, self.view.frame.size.width/2 + 19, self.view.frame.size.height)];
             }
             event.stageNum = (NSNumber*) ts.eventDict[key][@"stage"];
             event.backgroundColor = [UIColor clearColor];
